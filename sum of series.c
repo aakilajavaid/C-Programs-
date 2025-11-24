08cc9dbe35fa5b4db9c13072ce86443bc6e868f3
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 18:34:10 +0530

    Create sum of series.c

---
+++ sum of series.c
@@ -0,0 +1,22 @@
+// Program to display sum of series
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long int x, sum;
+
+    x = 2;    
+    sum = 0; 
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum= sum+x;    
+        x = x * x;   
+    }
+
+    printf("The sum of the series = %lld\n", sum);
+
+    return 0;
+}
