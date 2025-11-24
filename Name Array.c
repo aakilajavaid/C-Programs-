6f95715314effb57d831183090dcd928975be461
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 19:05:59 +0530

    Create Name Array.c

---
+++ Name Array.c
@@ -0,0 +1,16 @@
+// Array name
+#include <stdio.h>
+
+int main()
+{
+    int i=0;
+    char str[i];
+    printf("Enter your name:\n");
+    scanf("%s", str);   
+    while (str[i] != '\0')   
+    {
+        i++;
+    }
+    printf("the lenth of your name is %d", i);
+    return 0;
+}
