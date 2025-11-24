3caac742fab4ae109b678b1c748d7a173c9e40ed
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 19:03:52 +0530

    Create array string.c

---
+++ array string.c
@@ -0,0 +1,17 @@
+// Array string
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    char str[100];
+    printf("Enter the string:\n");
+    scanf("%s", str);   
+    printf("The entered string is:\n");
+    while (str[i] != '\0')   
+    {
+        printf("%c", str[i]);
+        i++;
+    }
+    return 0;
+}
