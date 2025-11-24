a4220980fbb80438a61d2914d36f0f1fc48e52b2
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 18:42:28 +0530

    Create Display pattern 5.c

---
+++ Display pattern 5.c
@@ -0,0 +1,17 @@
+//Display the following pattern
+#include<stdio.h>
+int main()
+{
+    int i,n,z;
+    printf("enter the number of terms: ");
+    scanf("%d",&n);
+    for (i=1; i<=n; i++)
+    {
+        for (z=1;z<=5;z++)
+        {
+            printf ("%d ",z);
+        }
+        printf("\t");
+    }
+    return 0;
+}
