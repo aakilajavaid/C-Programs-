e8dd51d111b5de6109a32a3286e8a0f63b5f7e9e
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 18:38:46 +0530

    Create checking positive or negative.c

---
+++ checking positive or negative.c
@@ -0,0 +1,20 @@
+// Program to check whether the number is positive or negative
+#include <stdio.h>
+int main ()
+{
+    float x;
+    printf("Enter the number:\n");
+    scanf("%f", &x);
+
+    if (x > 0) {
+        printf("%f is positive", x);
+    }
+    else if (x < 0) {
+        printf("%f is negative", x);
+    }
+    else {
+        printf("Zero");
+    }
+
+    return 0;
+}
