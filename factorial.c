95d9ac4013198c29c5b3b136cf59edb421d37f3e
aakilajavaid <aakilajavaid1@gmail.com>
2025-11-24 18:44:45 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,18 @@
+// Factorial of a number
+#include <stdio.h>
+int main()
+{
+    int i, n, product;
+    product = 1; // initialization
+
+    printf("enter the number of series elements to be multuplied:\n");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        product = product * i;
+    }
+
+    printf("the factorial of the given numbers is :%d ", product);
+    return 0;
+}
